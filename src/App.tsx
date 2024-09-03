f505508abcad149e65d2632e77955f8358718a0e
Ayush <ayushwilltry@gmail.com>
2024-09-03 13:15:09 +0545

    asdasdsad

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 // import { useEffect, useRef } from "react";
-import ChatBot from "./Chatbot";
-import {showMessenger, hideMessenger, showDialog, hideDialog } from 'react-messenger-chat-plugin';
+// import {showMessenger, hideMessenger, showDialog, hideDialog } from 'react-messenger-chat-plugin';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const App = () => {
     return (
         <>
-            <div id="fb-root"></div>
-            <div className="App">
-              <button onClick={() => {showMessenger(true)}}>show messenger</button>
-              <button onClick={() => {hideMessenger()}}>hide messenger</button>
-              <button onClick={() => {showDialog()}}>show dialog</button>
-              <button onClick={() => {hideDialog()}}>hide dialog</button>
-              {/* <button onclick={() => {setMessengerBottomSpacing(100)}}>set chat 100px in bottom spacing<button> */}
+            
 
-              {/* <MessengerChat pageId='123456789101112' /> */}
-            </div>
-            <ChatBot />
-            {/* <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div> */}
+            <MessengerCustomerChat
+              pageId="209191082594903"
+              appId="714647430026707"
+              htmlRef="<REF_STRING>"
+            />
         </>
     );
     }; 
