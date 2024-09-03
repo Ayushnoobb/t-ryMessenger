7f5369df8c2b70a3dec42c95ed2609a6007954c6
Ayush <ayushwilltry@gmail.com>
2024-09-03 13:04:32 +0545

    asdasdasdasda

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 // import { useEffect, useRef } from "react";
 import ChatBot from "./Chatbot";
+import {showMessenger, hideMessenger, showDialog, hideDialog } from 'react-messenger-chat-plugin';
 
 const App = () => {
-    // const MessengerRef = useRef<any>();
-    // useEffect(() => {
-    //     if (MessengerRef.current) {
-    //         MessengerRef.current.setAttribute('page_id', 209191082594903);
-    //         MessengerRef.current.setAttribute('attribution', 'biz_inbox');
-    //     }
-    
-    //     window.fbAsyncInit =  () => {
-    //         window.FB.init({
-    //             appId : 714647430026707 ,
-    //             xfbml: true,
-    //             version: 'v16.0',
-    //         });
-    //     };
-    
-    //     (function (d, s, id) {
-    //         let js: HTMLScriptElement | null,
-    //             fjs = d.getElementsByTagName(s)[0];
-    //         if (d.getElementById(id)) return;
-    //         js = d.createElement(s) as HTMLScriptElement;
-    //         js.id = id;
-    //         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    //         fjs.parentNode?.insertBefore(js, fjs);
-    //     })(document, 'script', 'facebook-jssdk');
-    // }, []);
-    // console.log(MessengerRef)
     return (
         <>
             <div id="fb-root"></div>
+            <div className="App">
+              <button onClick={() => {showMessenger(true)}}>show messenger</button>
+              <button onClick={() => {hideMessenger()}}>hide messenger</button>
+              <button onClick={() => {showDialog()}}>show dialog</button>
+              <button onClick={() => {hideDialog()}}>hide dialog</button>
+              {/* <button onclick={() => {setMessengerBottomSpacing(100)}}>set chat 100px in bottom spacing<button> */}
+
+              {/* <MessengerChat pageId='123456789101112' /> */}
+            </div>
             <ChatBot />
             {/* <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div> */}
         </>
